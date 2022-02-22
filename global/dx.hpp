71b03946847fd45bfeecff58e4465bac2f19d668
srao39 <79939415+srao39@users.noreply.github.com>
2022-02-22 11:43:24 -0700

    Add files via upload

---
+++ global/dx.hpp
@@ -0,0 +1,17 @@
+/// Copyright Cornerstone; all rights reserved
+
+#pragma once
+
+// Direct Graphics
+#include <DXGI.h>
+#pragma comment( lib, "dxgi.lib" )
+#include <D2D1.h>
+#pragma comment( lib, "d2d1.lib" )
+#include <d3d9.h>
+#pragma comment( lib, "d3d9.lib" )
+#include <d3dx9.h>
+#pragma comment( lib, "d3dx9" dlib )
+#include <D3D11.h>
+#pragma comment( lib, "d3d11.lib" )
+#include <D3DX11.h>
+#pragma comment( lib, "d3dx11" dlib )
