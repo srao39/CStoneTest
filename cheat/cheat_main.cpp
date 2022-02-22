517b2b012487d8e4de05c229362239278f4b263d
srao39 <79939415+srao39@users.noreply.github.com>
2022-02-22 11:24:01 -0700

    Add files via upload

---
+++ cheat/cheat_main.cpp
@@ -0,0 +1,13 @@
+/// Copyright Cornerstone; all rights reserved
+
+#include "pch.hpp"
+
+BOOL WINAPI DllMain( HINSTANCE const inst,
+					 DWORD const reason,
+					 LPVOID const /* reserved */ )
+{
+	( void )inst;
+	( void )reason;
+
+	return TRUE;
+}
---
+++ cheat/pch.hpp
@@ -0,0 +1,6 @@
+/// Copyright Cornerstone; all rights reserved
+
+#pragma once
+
+#include "shared/pch.hpp"
+#include "shared/face.hpp"
